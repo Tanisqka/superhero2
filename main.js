bb41180255d1e52d7c79ed91617d4a145037b08b
Tanisqka <74412417+Tanisqka@users.noreply.github.com>
2021-02-10 23:05:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+canvas = new fabric.Canvas("myCanvas");
+player_x = 10;
+player_y = 10;
+image_width = 30;
+image_height = 30;
+var player_object = "";
+var image_object = "";
+
+function player_update() {
+    fabric.Image.fromURL("player.png", function(Img) {
+        player_object = Img;
+
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top: player_y,
+            left: player_x
+        });
+        canvas.add(player_object);
+    });
+}
